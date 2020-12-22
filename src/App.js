ec386c934b5c520433b0d88e6cc00c0a35046713
Sagi Weizmann <sagiweizmann@gmail.com>
2020-12-22 13:52:37 +0200

    Updated README.md

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Form ({ option }) {
+	return (
+		<form className='account-form' onSubmit={(evt) => evt.preventDefault()}>
+			<div className={'account-form-fields ' + (option === 1 ? 'sign-in' : (option === 2 ? 'sign-up' : 'forgot')) }>
+				<input id='email' name='email' type='email' placeholder='E-mail' required />
+				<input id='password' name='password' type='password' placeholder='Password' required={option === 1 || option === 2 ? true : false} disabled={option === 3 ? true : false} />
+				<input id='repeat-password' name='repeat-password' type='password' placeholder='Repeat password' required={option === 2 ? true : false} disabled={option === 1 || option === 3 ? true : false} />
+			</div>
+			<button className='btn-submit-form' type='submit'>
+				{ option === 1 ? 'Sign in' : (option === 2 ? 'Sign up' : 'Reset password') }
+			</button>
+		</form>
+	)
+}
+
+function App () {
+	const [option, setOption] = React.useState(1)
+	
+	return (
+		<div className='container'>
+			<header>
+				<div className={'header-headings ' + (option === 1 ? 'sign-in' : (option === 2 ? 'sign-up' : 'forgot')) }>
+					<span>Sign in to your account</span>
+					<span>Create an account</span>
+					<span>Reset your password</span>
+				</div>
+			</header>
+			<ul className='options'>
+				<li className={option === 1 ? 'active' : ''} onClick={() => setOption(1)}>Sign in</li>
+				<li className={option === 2 ? 'active' : ''} onClick={() => setOption(2)}>Sign up</li>
+				<li className={option === 3 ? 'active' : ''} onClick={() => setOption(3)}>Forgot</li>
+			</ul>
+			<Form option={option} />
+		</div>
+	)
 }
 
 export default App;
